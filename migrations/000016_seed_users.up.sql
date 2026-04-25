7ccedfdeb55ab68f336ecd5b0e3af2b740e40e5c
forgedynasties <alihassan.pekka@gmail.com>
2026-04-25 17:31:47 +0500

    migration17

--- migrations/000016_seed_users.up.sql
+++ migrations/000016_seed_users.up.sql
@@ -1,5 +1,5 @@
 INSERT INTO users (username, display_name, password_hash, is_admin)
 VALUES
-  ('ali',     'Ali',     '$2a$12$zVEwHu5NtVBpQgBUvGI2m.axswbej9M0DUULQNFEv7B2mdopVPtRC', TRUE),
-  ('alishba', 'Alishba', '$2a$12$Juy8xz3MAsSoSxkA3AzbdOANym.O9Tam/vVIfBDmw3OLCAt5ls9CC', FALSE)
+  ('ali',     'Ali',     '$2a$12$t/OrPmjZBEfQtOd3lwmf7eHOqbeVIvmqgMG7J4ylIEjWCKSJs3Em6', TRUE),
+  ('alishba', 'Alishba', '$2a$12$f2Ts0CUdSpFH3DFmLeKKCeSDa5mRhFQr/kaWjo.WBbNw05A6u26ta', FALSE)
 ON CONFLICT (username) DO NOTHING;
---
+++ migrations/000017_fix_user_passwords.up.sql
@@ -0,0 +1,2 @@
+UPDATE users SET password_hash = '$2a$12$t/OrPmjZBEfQtOd3lwmf7eHOqbeVIvmqgMG7J4ylIEjWCKSJs3Em6' WHERE username = 'ali';
+UPDATE users SET password_hash = '$2a$12$f2Ts0CUdSpFH3DFmLeKKCeSDa5mRhFQr/kaWjo.WBbNw05A6u26ta' WHERE username = 'alishba';
